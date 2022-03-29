8a08836c617c0f8b851837478ec1dba1a2fbd0d4
Andru Stefanescu <als209@cam.ac.uk>
2022-03-29 23:56:15 +0300

    Refractor phone app
    Signed-off-by: Andru Stefanescu <als209@cam.ac.uk>

--- phone-app/ios/Runner/background/ConnectionService.swift
+++ phone-app/ios/Runner/background/ConnectionService.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 class ConnectionService {
-    private static let URL_STRING: String = "http://10.4.1.86:8080/"
+    private static let URL_STRING: String = "http://127.0.0.1:8080/"
     
     public static func getDistances(userId: String, partial: Bool) -> Array<NewDistanceMessage> {
         var url: String = URL_STRING
@@ -51,6 +51,33 @@ class ConnectionService {
             task.resume()
             lock.lock()
         }
+    }
+    
+    private static func postObjectWithResp(json: Data, endpoint: String) -> String {
+        print("intra")
+        var request: URLRequest = URLRequest(url: URL(string: URL_STRING + endpoint)!)
+        var resp:String = ""
+        request.httpMethod = "POST"
+        request.httpBody = json
+        request.timeoutInterval = 180
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        let lock: NSLock = NSLock()
+        
+        lock.lock()
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            resp = String(decoding: data!, as: UTF8.self)
+            lock.unlock()
+        }
+        task.resume()
+        lock.lock()
+        return resp
+    }
+    
+    public static func sendJSONBody(message: JSONBody) -> String {
+        let json: Data = try! JSONEncoder().encode(message)
+        
+        return postObjectWithResp(json: json, endpoint: "distance-calculator")
     }
     
     public static func sendNewLocation(message: LocationUploadMessage) {
