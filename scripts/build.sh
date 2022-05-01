468198a22a231748eda22871dc9080c0f653d667
Andru Stefanescu <als209@cam.ac.uk>
2022-05-01 18:24:31 +0100

    Remove unused scripts
    Signed-off-by: Andru Stefanescu <als209@cam.ac.uk>

--- scripts/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd he-component/bridge/jni
-cmake .
-make
-ls -la
--- scripts/install_seal.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd $HOME
-git clone https://github.com/microsoft/SEAL.git
-cd SEAL/
-cmake -S . -B build/ -DSEAL_USE_MSGSL=OFF -DSEAL_USE_ZLIB=OFF -DSEAL_USE_ZSTD=OFF
-cmake --build build
-sudo cmake --install build
